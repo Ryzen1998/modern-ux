eb0da9ee9fae5d8e5f5f1adb9b283685568999f3
Ryzen1998 <apexashwin@gmail.com>
2022-08-05 10:35:35 +0300

    file and folder structer completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
 import React from 'react'
+import NavBar from './components/navbar/NavBar'
+import Header from './container/header/Header'  
+import Brand from './components/brand/Brand'
+import WhatGPT3 from './container/whatGPT3/WhatGpt3'
+import Features from './container/features/Features'
+import Possibility from './container/possibility/Possibility'
+import Cta from './components/cta/Cta'
+import Blog from './container/blog/Blog'
+import Footer from './container/footer/Footer'
+
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+     <div className="gradient_bg">
+      <NavBar/>
+      <Header/>
+     </div>
+     <Brand/>
+     <WhatGPT3/>
+     <Features/>
+     <Possibility/>
+     <Cta/>
+     <Blog/>
+     <Footer/>
+
+
+
+    </div>
   )
 }
 
